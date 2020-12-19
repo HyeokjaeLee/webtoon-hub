93d20a503c2485694be0cf5ad92e75b42eeb10d3
leehyeokjae <leehyeokjae97@gmail.com>
2020-12-19 21:44:12 +0900

    ver 0.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const webtoon_api_url = "https://toy-projects-api.herokuapp.com/webtoon/all";
+
+const get_json_data = (url: string) => {
+  let xmlhttp = new XMLHttpRequest();
+  let json_data: string="";
+  xmlhttp.onreadystatechange = () => {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      try {
+        json_data = JSON.parse(xmlhttp.responseText);
+      } catch (err) {
+        console.log(err.message + " in " + xmlhttp.responseText);
+        return;
+      }
+    }
+  };
+  xmlhttp.open("GET", url, false); //true는 비동기식, false는 동기식 true로 할시 변수 변경전에 웹페이지가 떠버림
+  xmlhttp.send();
+  return json_data;
+};
 
 function App() {
   return (
