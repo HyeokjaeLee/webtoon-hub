69ce37f51c3eaa7568ad974233b8336f1c20f3e8
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-11-21 07:52:26 +0900

    💄 refactor: elements 추가
    * 검색, 플랫폼 리스트 추가
    * 대략적인 UI 구현

--- src/layouts/global.tsx
+++ src/layouts/global.tsx
@@ -1,6 +1,9 @@
 import "styles/global.scss"
 import React, { useState, useEffect } from "react"
 import { WebtoonData } from "contexts/webtoon-data"
+import KakaoWebtoon from "img/kakao-webtoon.svg"
+import KakaoPage from "img/kakao-page.svg"
+import NaverWebtoon from "img/naver-webtoon.svg"
 import axios from "axios"
 export default function TemplateWrapper({ children }: any) {
   const [data, setData]: [Webtoon.State, Webtoon.SetState] = useState(undefined)
@@ -10,10 +13,35 @@ export default function TemplateWrapper({ children }: any) {
 
   return (
     <WebtoonData.Provider value={{ data, setData }}>
-      <nav>
+      <nav className="main-nav">
         <h1>WEBTOON HUB</h1>
+        <ul className="platform-list">
+          <li>Search</li>
+          <li>Platform</li>
+        </ul>
+        <ul className="platform-list">
+          <li>
+            <NaverWebtoon />
+            Naver Webtoon
+          </li>
+          <li>
+            <KakaoWebtoon />
+            Kakao Webtoon
+          </li>
+          <li>
+            <KakaoPage />
+            Kakao Page
+          </li>
+          <li>
+            <span className="all-img">A</span>
+            All Platform
+          </li>
+        </ul>
       </nav>
       <main>{children}</main>
+      <footer>
+        © HyeokjaeLee. All rights reserved. Powered by GitHub Pages.
+      </footer>
     </WebtoonData.Provider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,54 @@
 import { WebtoonData } from "contexts/webtoon-data"
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import Webtoon from "components/webtoon"
 export default function Home() {
   const webtoonContext = useContext(WebtoonData)
-  const webtoonData = webtoonContext?.data?.slice(0, 100)
-  if (!!webtoonData) {
+
+  if (!!webtoonContext?.data) {
+    const [webtoonData, setWebtoonData] = useState(webtoonContext.data)
+    useEffect(() => {
+      let todayWeekNum = new Date().getDay()
+      todayWeekNum = todayWeekNum === 0 ? 6 : todayWeekNum - 1
+      setWebtoonData(
+        webtoonData.filter(webtoon => webtoon.week === todayWeekNum)
+      )
+    }, [])
     return (
-      <ul className="webtoon-list">
-        {webtoonData.map(webtoon => (
-          <Webtoon webtoonData={webtoon} />
-        ))}
-      </ul>
+      <>
+        <nav className="sub-nav">
+          <ul className="week-list">
+            <li>
+              <a>월</a>
+            </li>
+            <li>
+              <a>화</a>
+            </li>
+            <li>
+              <a>수</a>
+            </li>
+            <li>
+              <a>목</a>
+            </li>
+            <li>
+              <a>금</a>
+            </li>
+            <li>
+              <a>토</a>
+            </li>
+            <li>
+              <a>일</a>
+            </li>
+            <li className="finished">
+              <a>완결</a>
+            </li>
+          </ul>
+        </nav>
+        <ul className="webtoon-list">
+          {webtoonData.map(webtoon => (
+            <Webtoon webtoonData={webtoon} />
+          ))}
+        </ul>
+      </>
     )
   } else {
     return <div></div>
