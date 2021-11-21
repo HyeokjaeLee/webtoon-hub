d3c784d9ef872240a117d3a0c3a78dd85cb9652d
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-11-22 00:43:53 +0900

    🎨 refactor: 개발 환경, 구조 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "assets/scss/index.scss";
+import axios from "axios";
+import { Button } from "reactstrap";
 function App() {
+  console.log("test");
+  test();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="test">
+      <div className="App">
+        <Button color="info">info</Button>
+        <Button color="danger">danger</Button>
+        <Button color="success">success</Button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+async function test() {
+  const res = await axios.get("https://korea-webtoon-api.herokuapp.com/all");
+  const data: Webtoon.Data = res.data;
+}
